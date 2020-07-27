57f15e494c40cd824d78b0493307f60b325edf01
Mohammed-Sameeruddin <65489551+Mohammed-Sameeruddin@users.noreply.github.com>
2020-07-27 13:05:44 +0530

    Update settings.py

--- myFirstProject/settings.py
+++ myFirstProject/settings.py
@@ -129,8 +129,8 @@ STATIC_ROOT = os.path.join(BASE_DIR,'assests')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'mohammedsameeruddin272001@gmail.com'
+EMAIL_HOST_USER = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = "mohammedsameeruddin1@"
+EMAIL_HOST_PASSWORD = ""
 ACCOUNT_EMAIL_VERIFICATION = 'none'
